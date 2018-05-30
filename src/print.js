a4593d9f0f66dea51bb954c826c0b1b32c909563
yuzhengbo <newobject@foxmail.com>
2018-05-30 14:12:59 +0800

    1

--- src/print.js
+++ src/print.js
@@ -1,6 +1,3 @@
 export default function printMe() {
-  console.log('这是一个Hello Wrold打印');
-  console.error('这是一个error打印');
-  console.log('现在可以使用web服务了');
-  console.log(22222);
+  console.error('这是一个错误打印');
 }
